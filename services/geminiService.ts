1c4d7e5f60908512d951764aa8b1d48efa84f10b
docnps95 <docnps95@gmail.com>
2025-12-24 08:24:09 +0530

    Clean up source files to leave only production build

--- services/geminiService.ts
+++
@@ -1,4 +0,0 @@
-// This service has been disabled for static site deployment.
-export const getDentalResponse = async (question: string, language: 'en' | 'hi'): Promise<string> => {
-  return "";
-};
